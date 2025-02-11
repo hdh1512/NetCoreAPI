caf4f2d512eb4b0b7fa8ab7f503251f22499fc94
hdh1512 <hhieu151203@gmail.com>
2025-02-11 20:58:55 +0700

    HTML_BT

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c45de0a86342e8034999a0c9e6413ada22de111")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b5ef91ea13996a8c604ca28f1521643c52e65f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
